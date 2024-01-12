2ff268bcc1936051008b311fda8c37ad0af303f6
NemanjaVukosavljevic <maze.vule@gmail.com>
2024-01-12 23:56:04 +0100

    homework 14

---
+++ D_14_p56q24/knjiga.js
@@ -0,0 +1,75 @@
+class Knjiga {
+    constructor(naslovArg, autorArg, godIzdanjaArg, brojStranaArg, cenaArg){
+        this.naslov = naslovArg;
+        this.autor = autorArg;
+        this.godIzdanja = godIzdanjaArg;
+        this.brojStrana = brojStranaArg;
+        this.cena = cenaArg;
+    };
+
+    // SETTER
+
+    set naslov(naslovArg){
+        this._naslov = naslovArg;
+    }
+
+    set autor(autorArg){
+        this._autor = autorArg;
+    }
+
+    set godIzdanja(godIzdanjaArg){
+        this._godIzdanja = godIzdanjaArg;
+    }
+
+    set brojStrana(brojStranaArg){
+        this._brojStrana = brojStranaArg;
+    }
+
+    set cena(cenaArg){
+        this._cena = cenaArg;
+    }
+
+    // GETTER
+
+    get naslov(){
+        return this._naslov;
+    };
+
+    get autor(){
+        return this._autor;
+    }
+
+    get godIzdanja(){
+        return this._godIzdanja;
+    }
+
+    get brojStrana(){
+        return this._brojStrana;
+    }
+
+    get cena(){
+        return this._cena;
+    }
+
+    // METODE
+
+    stampaj(){
+        console.log(`Naslov: ${this.naslov}, Autor: ${this.autor}, Godina izdanja: ${this.godIzdanja}, Broj stranica: ${this.brojStrana}, Cena knjige: ${this.cena}`);
+    };
+
+    obimna(){
+        return (this.brojStrana > 600) ? true : false;
+    };
+
+    skupa(){
+        return (this.cena > 8000) ? true : false;
+    };
+
+    dugackoIme(){
+        return (this.autor.length > 18) ? true : false;
+    }
+    
+};
+
+
+export default Knjiga;
---
+++ D_14_p56q24/main.js
@@ -0,0 +1,55 @@
+import Knjiga from "./knjiga.js";
+
+let knjiga1 = new Knjiga(`Juzni vetar`, `Nemanja Vukosavljevic`, 2023, 601, 8500);
+let knjiga2 = new Knjiga(`Pink Panter`, `Olivera Cirkovic`, 2020, 255, 3500);
+let knjiga3 = new Knjiga(`Esmeralda 973`, `Aleksandar Bilanovic`, 2017, 150, 1900);
+
+let nizKnjiga = [knjiga1, knjiga2, knjiga3];
+
+
+console.log(`-----------------`);
+
+nizKnjiga.forEach(element => {
+    if (element.dugackoIme()){
+        console.log(`Dugacko ime ima ${element.autor}`);
+    }
+
+    if (element.skupa() && element.obimna()){
+        console.log(`${element.naslov} je i skupa i obimna!`);
+    }
+});
+
+
+console.log(`-----------------`);
+
+const ukupnaCena = (array) => {
+    let ukupno = 0;
+    array.forEach(element => {
+        ukupno += element.cena;
+    });
+
+    return ukupno;
+};
+
+
+const prosecnaCena = (array) => {
+    let ukupno = ukupnaCena(array);
+
+    return ukupno / array.length;
+}
+
+
+const prosecnaStranica = (array) => {
+    let ukupnaCenaSvihKnjiga = ukupnaCena(array);
+
+    let brojStranica = 0;
+
+    array.forEach((element) => {
+        brojStranica += element.brojStrana;
+    });
+
+    return ukupnaCenaSvihKnjiga / brojStranica;
+}
+
+
+
